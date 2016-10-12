4cf230a38474fefbac1f8912e438241f985e5e09
vineethasasi <vineetha.sasi@dhilogics.com>
2016-10-12 12:18:53 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,20 @@
+import org.json.simple.JSONObject;
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+		JSONObject obj=new JSONObject();
+		obj.put("name","vinee");
+		obj.put("number","1234");
+		obj.put("is_vip","yes");
+		System.out.print(obj);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
